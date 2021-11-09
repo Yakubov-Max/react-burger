bdd10ce9a3b57cbfa4ce119a158a4d20460f2b43
Max <55656370+Yakubov-Max@users.noreply.github.com>
2021-11-09 20:03:21 +0300

    feature: BurgerConstructor

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
-import AppHeader from '../app-header/AppHeader'
+import AppHeader from '../app-header/AppHeader';
+import BurgerConstructor from "../burger-constructor/BurgerConstructor";
+import BurgerIngridients from '../burger-ingridients/BurgerIngridients';
+import data from "../utils/data.json"
 
 function App() {
   return (
     <div className="App">
       <AppHeader></AppHeader>
+      <main style={{display: 'flex', justifyContent: 'center'}}>
+        <BurgerIngridients />
+        <BurgerConstructor data={data}/>
+      </main>
     </div>
   );
 }
---
+++ src/burger-ingridients/BurgerIngridients.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ingridientsStyles from "./BurgerIngridients.module.css"
+
+function BurgerIngridients() {
+  return (
+    <section className={ingridientsStyles.container}>
+
+    </section>
+  )
+}
+
+export default BurgerIngridients
